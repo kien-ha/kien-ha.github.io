dcbc482c9b3735e18c5273203b20f77ffe2528d2
Kien Ha <kienha9922@gmail.com>
2018-07-16 18:08:14 -0400

    Make templates for header and footer
    Header and footer are both reused throughout the website. Make these
    components consistent and easier to maintain via PHP
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$header = file_get_contents("html/header.html");
+$footer = file_get_contents("html/footer.html");
+echo $header;
+?>
+<div id="container">
+    <div id="content">
+        <!-- content -->
+    </div>
+</div>
+<?php
+echo $footer;
+?>
