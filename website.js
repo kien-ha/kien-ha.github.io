904a03b7aba18a676a2f49f32d8f6d5cd237f571
Kien Ha <kienha9922@gmail.com>
2018-07-21 13:05:41 -0400

    Dynamically load blog posts on blog
    Blog posts linked too another HTML file, which after clicking on the
    link, brought you to another page. This causes the user to have to load
    a new page. Instead, clicking on the blog links dynamically loads the
    posts into the page, causing the user to not have to load a new page to
    see posts.
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

---
+++ website.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function() {
+    $("#blog-2").click(function() {
+        $("#blog-2").load("blogs/2018-06-05-remove-persistent-running-app-in-notification-background.html #blogPost")
+    });
+});
+
+$(document).ready(function() {
+    $("#blog-1").click(function() {
+        $("#blog-1").load("blogs/2016-08-29-thoughts-on-internship-with-OpenDayLight.html #blogPost")
+    });
+});
