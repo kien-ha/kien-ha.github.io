fe8b1331e0b683cc4455192c43474e56f4bc4159
Kien Ha <kienha9922@gmail.com>
2018-07-22 01:39:51 -0400

    Create function setAllBlogLink
    This function sets each blog post links to a unique ID
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- website.js
+++ website.js
@@ -1,13 +1,22 @@
 "use strict";
 
+function setAllBlogId() {
+    let blogLinkElements = document.querySelectorAll('.blog-link');
+    for(let i = blogLinkElements.length - 1; i >= 0; i--) {
+        blogLinkElements[i].id = 'blog-' + (i+1);
+    }
+}
+
+setAllBlogId();
+
 $(document).ready(function() {
-    $("#blog-2").click(function() {
-        $("#blog-2").load("blogs/2018-06-05-remove-persistent-running-app-in-notification-background.html #blogPost")
+    $("#blog-1").click(function() {
+        $("#blog-1").load("blogs/2018-06-05-remove-persistent-running-app-in-notification-background.html #blogPost")
     });
 });
 
 $(document).ready(function() {
-    $("#blog-1").click(function() {
-        $("#blog-1").load("blogs/2016-08-29-thoughts-on-internship-with-OpenDayLight.html #blogPost")
+    $("#blog-2").click(function() {
+        $("#blog-2").load("blogs/2016-08-29-thoughts-on-internship-with-OpenDayLight.html #blogPost")
     });
 });
